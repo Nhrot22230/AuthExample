d293e6915101761ba0736b3052b9e69c77c32a5b
Piero Pastor <a20210836@pucp.edu.pe>
2024-11-03 00:32:53 -0500

    Update EstudianteRiesgoController.php

--- app/Http/Controllers/EstudianteRiesgoController.php
+++ app/Http/Controllers/EstudianteRiesgoController.php
@@ -36,9 +36,9 @@ class EstudianteRiesgoController extends Controller
         $fechaActual = new DateTime();
         $ciclo = Semestre::where('estado', 'activo')->first();
         $periodo = $ciclo->anho . "-" . $ciclo->periodo;
+        return response()->json("hjasjksdahjkasd");
         foreach ($estudiantesRiesgo as $estudiante)
         {
-            return response()->json("hjasjksdahjkasd");
             $est = Usuario::find(Estudiante::where('codigoEstudiante', $estudiante->codigo_estudiante)->first()->usuario_id);
 
             if($periodo != $estudiante->ciclo) continue;
