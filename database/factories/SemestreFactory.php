55f75f99ef7c9b1687ede3e35f58614c6b136fa3
JhairtVegaQ <116320255+JhairtVegaQ@users.noreply.github.com>
2024-11-04 17:16:29 -0500

    tld dance sofia

--- database/factories/SemestreFactory.php
+++ database/factories/SemestreFactory.php
@@ -17,11 +17,11 @@ class SemestreFactory extends Factory
      */
     public function definition(): array
     {
-        $anho = $this->faker->numberBetween(2010, 2025);
+        $anho = $this->faker->numberBetween(2022, 2025);
         $periodo = $this->faker->randomElement(['0', '1', '2']);
         
         while (Semestre::where('anho', $anho)->where('periodo', $periodo)->exists()) {
-            $anho = $this->faker->numberBetween(2020, 2025);
+            $anho = $this->faker->numberBetween(2022, 2025);
             $periodo = $this->faker->randomElement(['0', '1', '2']);
         }
 
