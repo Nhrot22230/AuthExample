86ff24bc292e58b7a40816f4039045181b871566
nhrot <a20213812@pucp.edu.pe>
2024-11-04 05:20:27 -0500

    Resolved Conflict After Merge

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -24,23 +24,34 @@ class PermissionSeeder extends Seeder
             ['name' => 'especialidades', 'access_path' => AccessPath::UNIDADES],
             ['name' => 'secciones', 'access_path' => AccessPath::UNIDADES],
             ['name' => 'cursos', 'access_path' => AccessPath::CURSOS],
-            ['name' => 'planes de estudio', 'access_path' => AccessPath::UNIDADES],
+            ['name' => 'planes de estudio', 'access_path' => AccessPath::PLAN_ESTUDIOS],
             ['name' => 'horarios', 'access_path' => AccessPath::CURSOS],
             ['name' => 'usuarios', 'access_path' => AccessPath::PERSONAS],
-            ['name' => 'roles', 'access_path' => AccessPath::PERSONAS],
-            ['name' => 'permisos', 'access_path' => AccessPath::PERSONAS],
+            ['name' => 'roles', 'access_path' => AccessPath::CONFIGURACION_SISTEMA],
+            ['name' => 'permisos', 'access_path' => AccessPath::CONFIGURACION_SISTEMA],
+            ['name' => 'tema de tesis', 'access_path' => AccessPath::PLAN_ESTUDIOS],
+            ['name' => 'jefes de práctica', 'access_path' => AccessPath::JEFE_PRACTICA],
+            ['name' => 'observaciones', 'access_path' => AccessPath::CURSOS],
+            ['name' => 'administrativos', 'access_path' => AccessPath::PERSONAS],
+            ['name' => 'estudiantes', 'access_path' => AccessPath::PERSONAS],
+            ['name' => 'docentes', 'access_path' => AccessPath::PERSONAS],
+            ['name' => 'jurados', 'access_path' => AccessPath::CURSOS],
+            ['name' => 'asesores', 'access_path' => AccessPath::CURSOS],
+            ['name' => 'solicitudes', 'access_path' => AccessPath::TRAMITES_ACADEMICOS],
         ];
 
         foreach ($permission_categories as $permission_category) {
             $category = PermissionCategory::create($permission_category);
-            $permissions = [
-                ['name' => 'ver ' . $permission_category['name'], 'category_id' => $category->id],
-                ['name' => 'manage ' . $permission_category['name'], 'category_id' => $category->id],
-            ];
+            
+            Permission::create([
+                'name' => 'ver ' . $permission_category['name'],
+                'category_id' => $category->id
+            ]);
 
-            foreach ($permissions as $permission) {
-                Permission::create($permission);
-            }
+            Permission::create([
+                'name' => 'manage ' . $permission_category['name'],
+                'category_id' => $category->id
+            ]);
         }
     }
 }
