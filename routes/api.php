115efe103756d764c05b69ba495c6153b8b1e6d8
nhrot <a20213812@pucp.edu.pe>
2024-11-06 13:11:48 -0500

    Update on api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 $dir = new RecursiveDirectoryIterator(__DIR__.'/api');
 $iterator = new RecursiveIteratorIterator($dir);
-$regex = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+$regex = new RegexIterator($iterator, '/^.+\.php$/i', RegexIterator::GET_MATCH);
 
 foreach ($regex as $files) {
     require_once $files[0];
