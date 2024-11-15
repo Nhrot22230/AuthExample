9485d5771830048a5bb53f8e597c29d071cf2d6c
nhrot <a20213812@pucp.edu.pe>
2024-11-15 14:26:45 -0500

    added fetch grupo criterios

--- app/Http/Controllers/Convocatorias/ConvocatoriaController.php
+++ app/Http/Controllers/Convocatorias/ConvocatoriaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Convocatorias;
 
 use App\Http\Controllers\Controller;
 use App\Models\Convocatorias\Convocatoria;
+use App\Models\Convocatorias\GrupoCriterios;
 use Illuminate\Http\Request;
 
 class ConvocatoriaController extends Controller
@@ -17,7 +18,7 @@ class ConvocatoriaController extends Controller
         $search = request('search', '');
         $seccion = request('seccion', null);
         $filters = request('filters', []);  // This will be an array of states
-    
+
         $convocatorias = Convocatoria::with('gruposCriterios', 'comite', 'candidatos')
             ->where('nombre', 'like', "%$search%")
             ->when($filters, function ($query, $filters) {
@@ -25,35 +26,49 @@ class ConvocatoriaController extends Controller
             })
             ->where('seccion_id', 'like', "%$seccion%")
             ->paginate($perPage);
-    
+
         return response()->json($convocatorias, 200);
     }
-    
+
+    public function indexCriterios($entity_id)
+    {
+        if (!is_numeric($entity_id)) {
+            return response()->json(['error' => 'Invalid entity ID.'], 400);
+        }
+
+        $perPage = request()->input('per_page', 10);
+        $search = request()->input('search', '');
+
+        $grupoCriterios = GrupoCriterios::with('convocatorias')
+            ->whereHas('convocatorias', function ($query) use ($entity_id) {
+                $query->where('seccion_id', $entity_id);
+            })
+            ->when($search, function ($query, $search) {
+                $query->where('nombre', 'like', "%{$search}%");
+            })
+            ->paginate($perPage)
+            ->appends(request()->only(['search', 'per_page']));
+
+        return response()->json($grupoCriterios, 200);
+    }
+
 
     public function listar_convocatorias_todas()
     {
         try {
             // Obtener todas las convocatorias, con sus relaciones
             $convocatorias = Convocatoria::with('gruposCriterios', 'comite', 'candidatos')->get();
-    
+
             // Si no se encuentran convocatorias, retornar mensaje adecuado
             if ($convocatorias->isEmpty()) {
                 return response()->json(['message' => 'No se encontraron convocatorias'], 404);
             }
-    
+
             // Devolver las convocatorias en formato JSON
             return response()->json($convocatorias, 200);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Ocurrió un error', 'details' => $e->getMessage()], 500);
         }
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -68,14 +83,6 @@ class ConvocatoriaController extends Controller
      * Display the specified resource.
      */
     public function show(Convocatoria $convocatoria)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Convocatoria $convocatoria)
     {
         //
     }
--- routes/api/convocatorias/convocatorias.php
+++ routes/api/convocatorias/convocatorias.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/convocatorias/index', [ConvocatoriaController::class, 'index']);
 Route::get('/convocatorias', [ConvocatoriaController::class, 'listar_convocatorias_todas']);
+Route::get('/convocatorias/criterios/{entity_id}', [ConvocatoriaController::class, 'indexCriterios']);
