67807c15210a8785013ebccc189795b6541496fb
nhrot <a20213812@pucp.edu.pe>
2024-11-15 01:53:45 -0500

    updates on usuarios middlewares

--- routes/api/usuarios/administrativos.php
+++ routes/api/usuarios/administrativos.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\Usuarios\AdministrativoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('administrativos', [AdministrativoController::class, 'index'])->middleware('can:ver usuarios');
-Route::post('administrativos', [AdministrativoController::class, 'store'])->middleware('can:manage usuarios');
-Route::post('administrativos/multiple', [AdministrativoController::class, 'storeMultiple'])->middleware('can:manage usuarios');
-Route::get('administrativos/{codAdministrativo}', [AdministrativoController::class, 'show'])->middleware('can:ver usuarios');
-Route::put('administrativos/{codAdministrativo}', [AdministrativoController::class, 'update'])->middleware('can:manage usuarios');
-Route::delete('administrativos/{codAdministrativo}', [AdministrativoController::class, 'destroy'])->middleware('can:manage usuarios');
+
+Route::middleware("can:usuarios")->group(function () {
+    Route::get('administrativos', [AdministrativoController::class, 'index']);
+    Route::post('administrativos', [AdministrativoController::class, 'store']);
+    Route::post('administrativos/multiple', [AdministrativoController::class, 'storeMultiple']);
+    Route::get('administrativos/{codAdministrativo}', [AdministrativoController::class, 'show']);
+    Route::put('administrativos/{codAdministrativo}', [AdministrativoController::class, 'update']);
+    Route::delete('administrativos/{codAdministrativo}', [AdministrativoController::class, 'destroy']);
+});
--- routes/api/usuarios/docentes.php
+++ routes/api/usuarios/docentes.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\Usuarios\DocenteController;
-use App\Http\Middleware\JWTMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::get('docentes', [DocenteController::class, 'index'])->middleware('can:ver usuarios');
-Route::post('docentes', [DocenteController::class, 'store'])->middleware('can:manage usuarios');
-Route::post('docentes/multiple', [DocenteController::class, 'storeMultiple'])->middleware('can:manage usuarios');
-Route::get('docentes/{codDocente}', [DocenteController::class, 'show'])->middleware('can:ver usuarios');
-Route::put('docentes/{codDocente}', [DocenteController::class, 'update'])->middleware('can:manage usuarios');
-Route::delete('docentes/{codDocente}', [DocenteController::class, 'destroy'])->middleware('can:manage usuarios');
+Route::middleware("can:usuarios")->group(function () {
+    Route::get('docentes', [DocenteController::class, 'index']);
+    Route::post('docentes', [DocenteController::class, 'store']);
+    Route::post('docentes/multiple', [DocenteController::class, 'storeMultiple']);
+    Route::get('docentes/{codDocente}', [DocenteController::class, 'show']);
+    Route::put('docentes/{codDocente}', [DocenteController::class, 'update']);
+    Route::delete('docentes/{codDocente}', [DocenteController::class, 'destroy']);
+});
