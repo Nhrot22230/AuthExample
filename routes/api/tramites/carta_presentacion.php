f1bfe7bc54d29cbf7e3553a8c5d8d1dafa63c2df
RenattoGarciaArce <renatto_02_29@hotmail.com>
2024-11-16 16:42:57 -0500

    Controller SolicitudCartaPresentacion terminado

--- routes/api/tramites/carta_presentacion.php
+++ routes/api/tramites/carta_presentacion.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Tramites\CartaPresentacionController;
 
-Route::prefix('cartas')->group(function () {
+/*Route::prefix('cartas')->group(function () {
     Route::post('/{idEstudiante}/filtrar-estudiante', [CartaPresentacionController::class, 'index']);
     Route::post('/{idDocente}/filtrar-docente', [CartaPresentacionController::class, 'indexDocente']);
     Route::post('/{idUsuario}/filtrar-director', [CartaPresentacionController::class, 'indexDirector']);
     Route::post('/{idUsuario}/filtrar-secretaria', [CartaPresentacionController::class, 'indexSecretaria']);
     Route::get('/crear/{idEstudiante}', [CartaPresentacionController::class, 'create']);
     Route::post('/{idEstudiante}', [CartaPresentacionController::class, 'store']);
-});
+});*/
