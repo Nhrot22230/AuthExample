05823e452984e22df6a648d834241fc3ea625eeb
JhairtVegaQ <116320255+JhairtVegaQ@users.noreply.github.com>
2024-11-16 23:36:18 -0500

    Update convocatorias.php

--- routes/api/convocatorias/convocatorias.php
+++ routes/api/convocatorias/convocatorias.php
@@ -7,16 +7,8 @@ Route::get('/convocatorias/index', [ConvocatoriaController::class, 'index']);
 Route::get('/convocatorias', [ConvocatoriaController::class, 'listarConvocatoriasTodas']);
 Route::get('/convocatorias/criterios/{entity_id}', [ConvocatoriaController::class, 'indexCriterios']);
 Route::post('/convocatorias', [ConvocatoriaController::class, 'store']);
-<<<<<<< HEAD
-Route::put('/convocatorias/{id}', [ConvocatoriaController::class, 'update']);
-Route::get('/convocatorias/{id}', [ConvocatoriaController::class, 'show']);
-Route::post('/convocatorias/criterios', [ConvocatoriaController::class, 'storeGrupoCriterios']);
-Route::put('/convocatorias/criterios/{id}', [ConvocatoriaController::class, 'updateGrupoCriterios']);
-Route::get('/convocatorias/{idConvocatoria}/{idCandidato}', [ConvocatoriaController::class, 'obtenerEstadoCandidato']);
-Route::put('/convocatorias/{idConvocatoria}/{idCandidato}', [ConvocatoriaController::class, 'cambiarEstadoMiembroComite']);
-=======
 Route::get('/convocatorias/{id}', [ConvocatoriaController::class, 'show']);
 Route::put('/convocatorias/{id}', [ConvocatoriaController::class, 'update']);
 Route::get('/convocatorias/{id}/candidatos', [ConvocatoriaController::class, 'getCandidatosByConvocatoria']);
 Route::get('/convocatorias/{idConvocatoria}/{idCandidato}', [ConvocatoriaController::class, 'obtenerEstadoCandidato']);
->>>>>>> dda2736893ebabb4892f2787a1d78be13e08119b
+Route::put('/convocatorias/{idConvocatoria}/{idCandidato}', [ConvocatoriaController::class, 'cambiarEstadoMiembroComite']);
