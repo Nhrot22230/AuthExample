8dba909a11f194e6c49cbbe9be8718bec943ea77
ErMo2 <a20190315@pucp.edu.pe>
2024-11-22 00:16:55 -0500

    push

--- routes/api/universidad/cursos.php
+++ routes/api/universidad/cursos.php
@@ -7,10 +7,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('cursos')->group(function () {
     Route::get('/', [CursoController::class, 'index']);
+    Route::post('/cursosDocente', [CursoController::class, 'obtenerCursosPorDocente']);
+    Route::post('/actualizar-delegado', [CursoController::class, 'actualizarDelegado']);
+    Route::post('/horarios', [CursoController::class, 'obtenerHorariosPorDocenteYCursos']);
+    Route::post('/detalle', [CursoController::class, 'obtenerCursoPorId']);
     Route::get('/paginated', [CursoController::class, 'indexPaginated']);
     Route::get('/codigo/{codigo}', [CursoController::class, 'getByCodigo']);
     Route::get('/{cursoId}/docentes', [CursoController::class, 'obtenerDocentesPorCurso']);
     Route::get('/{cursoId}/horarios', [CursoController::class, 'obtenerHorariosPorCurso']);
+    Route::post('/alumnos', [CursoController::class, 'obtenerAlumnosPorHorario']);
     Route::get('/{entity_id}', [CursoController::class, 'show']);
 
     Route::middleware("can:unidades")->group(function () {
@@ -18,14 +23,9 @@ Route::prefix('cursos')->group(function () {
         Route::put('/{entity_id}', [CursoController::class, 'update']);
         Route::delete('/{entity_id}', [CursoController::class, 'destroy']);
     });
-
+    
     Route::middleware(AuthzMiddleware::class . ":cursos," . Curso::class)->group(function () {
         Route::put('/{entity_id}', [CursoController::class, 'update']);
         Route::delete('/{entity_id}', [CursoController::class, 'destroy']);
-        Route::get('/{entity_id}/cursosDocente', [CursoController::class, 'obtenerCursosPorDocente']);
-        Route::get('/{entity_id}/detalle', [CursoController::class, 'obtenerCursoPorId']);
-        Route::get('/{entity_id}/horarios', [CursoController::class, 'obtenerHorariosPorDocenteYCursos']);
-        Route::get('/{entity_id}/alumnos', [CursoController::class, 'obtenerAlumnosPorHorario']);
-        Route::post('/{entity_id}/actualizar-delegado', [CursoController::class, 'actualizarDelegado']);
     });
 });
