57b492fc4c7441f86520dc8a6e34342baeb59ad7
ErMo2 <a20190315@pucp.edu.pe>
2024-12-08 15:23:22 -0500

    cambio rutas

--- routes/api/estudiante_riesgo/estudiantes_riesgo.php
+++ routes/api/estudiante_riesgo/estudiantes_riesgo.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('estudiantesRiesgo/listar_profesor', [EstudianteRiesgoController::class, 'listar_por_especialidad_profesor']);
-Route::get('estudiantesRiesgo/listar_director', [EstudianteRiesgoController::class, 'listar_por_especialidad_director']);
-Route::post('estudiantesRiesgo/listar_informes', [EstudianteRiesgoController::class, 'listar_informes_estudiante']);
+Route::get('estudiantesRiesgoDA/listar_director/GA', [EstudianteRiesgoController::class, 'listar_por_especialidad_director']);
+Route::post('estudiantesRiesgoDA/listar_informes/GA', [EstudianteRiesgoController::class, 'listar_informes_estudiante']);
 Route::put('estudiantesRiesgo/actualizar_informe', [EstudianteRiesgoController::class, 'actualizar_informe_estudiante']);
-Route::post('estudiantesRiesgo/carga_alumnos', [EstudianteRiesgoController::class, 'carga_alumnos_riesgo']);
+Route::post('estudiantesRiesgoDA/carga_alumnos', [EstudianteRiesgoController::class, 'carga_alumnos_riesgo']);
 Route::post('estudiantesRiesgo/manage_informes', [EstudianteRiesgoController::class, 'manage_informes']);
 Route::get('estudiantesRiesgo/obtener_datos_semana', [EstudianteRiesgoController::class, 'obtener_datos_semana']);
 Route::get('estudiantesRiesgo/obtener_estadisticas_informes', [EstudianteRiesgoController::class, 'obtener_estadisticas_informes']);
@@ -16,9 +16,9 @@ Route::get('estudiantesRiesgo/listar_informes_director', [EstudianteRiesgoContro
 Route::get('estudiantesRiesgo/listar_semanas_existentes/{id}', [EstudianteRiesgoController::class, 'listar_semanas_existentes']);
 Route::delete('estudiantesRiesgo/eliminar_semana', [EstudianteRiesgoController::class, 'eliminar_semana']);
 Route::post('estudiantesRiesgo/crear-por-especialidad', [EstudianteRiesgoController::class, 'crearInformePorEspecialidad']);
-Route::post('estudiantesRiesgo/semanas-por-especialidad', [EstudianteRiesgoController::class, 'obtenerSemanasPorEspecialidad']);
-Route::get('estudiantesRiesgo/estadisticas', [EstudianteRiesgoController::class, 'obtenerEstadisticas']);
+Route::post('estudiantesRiesgoDA/semanas-por-especialidad', [EstudianteRiesgoController::class, 'obtenerSemanasPorEspecialidad']);
+Route::get('estudiantesRiesgoDA/estadisticas', [EstudianteRiesgoController::class, 'obtenerEstadisticas']);
 Route::get('estudiantesRiesgo/cursos', [EstudianteRiesgoController::class, 'obtenerCursos']);
 Route::post('estudiantesRiesgo/comparacion-cursos', [EstudianteRiesgoController::class, 'compararCursos']);
-Route::get('estudiantesRiesgo/ultimos-informes', [EstudianteRiesgoController::class, 'obtenerUltimosInformes']);
+Route::get('estudiantesRiesgoDA/ultimos-informes', [EstudianteRiesgoController::class, 'obtenerUltimosInformes']);
 
