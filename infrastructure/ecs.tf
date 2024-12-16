2f2a1a49623ce6a1a8a6650c8f15bca1d083ebe8
Daniel Ernesto Rivas Pareja <eselrey551@gmail.com>
2024-12-16 04:39:23 -0500

    Fix target group type to ip

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -51,6 +51,7 @@ resource "aws_lb_target_group" "app_tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
+  target_type = "ip"
 }
 
 resource "aws_lb_listener" "app_listener" {
