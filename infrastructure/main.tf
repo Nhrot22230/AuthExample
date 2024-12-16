9bb43943553cdd79d2e3df9ce481f5cb2283b186
Daniel Ernesto Rivas Pareja <eselrey551@gmail.com>
2024-12-16 04:55:14 -0500

    backend remoto tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "mi-bucket-de-estado-terraform"
+    key    = "estado.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
